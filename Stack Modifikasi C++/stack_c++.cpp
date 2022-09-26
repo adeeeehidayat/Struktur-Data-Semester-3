079fb9eb73fb261ba0a0cc40f7025fcad64d633d
AdeHidayat <adeeehidayat63@gmail.com>
2022-09-26 12:28:24 +0700

    upload tugas

--- Stack Modifikasi C++/stack_c++.cpp
+++ Stack Modifikasi C++/stack_c++.cpp
@@ -1,74 +1,113 @@
-#include <iostream>
-#include <stdlib.h> // menggunakan system("cls")
-#include <conio.h> // menggunakan getch()
-#define MAX 10 // batas stack yang ingin dibuat
-using namespace std;
+#include<iostream>
+#include<conio.h>
 
-int top=-1, Stack[MAX];
-void push(){
-    if(top == MAX-1){
-        cout << ">> Stack sudah Penuh! <<" << endl;
-    }else{
-        top++;
-        cout << ">> Masukan Data : ";
-        cin >> Stack[top];
-        cout << "Pada Index ke '" << top << "' Data [" << Stack[top] << "] Berhasil Ditambah" << endl;
-    }
+using namespace std;
+struct paket
+{
+    char jenis_brg[30];
+    int berat;
+    char kota[30];
+};
+
+struct paket kirim[100];
+
+/*******************************************/
+
+void tambahdata(int x)
+{
+    cout<<"Inputkan Jenis Barang    : ";
+    cin>>kirim[x].jenis_brg;
+    cout<<"Inputkan Berat           : ";
+    cin>>kirim[x].berat;
+    cout<<"Inputkan Kota Tujuan     : ";
+    cin>>kirim[x].kota;
 }
-void pop(){
-    if(top == -1){
-        cout << ">> Stack Kosong! <<" << endl;
-    }else{
-        cout << "Data [" << Stack[top] << "] pada index ke '" << top << "' Diambil !" << endl;
-        Stack[top--];
+
+void tampildata(int x)
+{
+    cout<<"=========================="<<endl;
+    cout<<"|| Daftar Kiriman Paket ||"<<endl;
+    cout<<"=========================="<<endl;
+    for(int a=1; a<x; a++)
+    {
+        cout<< "|| "<<a<<". ";
+        cout<<kirim[a].jenis_brg<<" ";
+        cout<<kirim[a].berat<<" ";
+        cout<<kirim[a].kota<<endl;
     }
+    cout<<">> Press Any Key To Continue... <<"<<endl<<endl;
+    getch();
+    /*harus Ijin --> #include<conio.h>*/
 }
-int clearr(){
-    return top = -1;
+
+void hapusdata(int x)
+{
+    cout<<"======================================"<<endl;
+    cout<<"Data Terakhir yang anda Hapus Adalah :"<<endl;
+    cout<<kirim[x].jenis_brg<<" ";
+    cout<<kirim[x].berat<<" Kg ";
+    cout<<kirim[x].kota<<endl;
+    cout<<"======================================"<<endl;
+    cout<<">> Press Any Key To Continue... <<"<<endl<<endl;
+    getch();
 }
-void Print(){
-    if (top == -1) {
-        cout << "          Stack : " << endl;
-        cout << "===========================" << endl;
-        cout << "          Kosong ! " << endl;
-        cout << "===========================" << endl;
-    }
-    else {
-        cout << "          Stack : " << endl;
-        cout << "===========================" << endl;
-        for (int i = top; i >= 0; i--){
-            cout << "     ===>[ " << Stack[i] << " ]<===" << endl;
-        }
-        cout << "===========================" << endl;
-    }
-}
+
 int main()
 {
-    int choose;
-    do {
-        Print();
-        cout << "\n1. Push\n2. Pop\n3. Clear\n4. Exit\nPergerakan ( TOP ) : " << top << "\n\n>> Masukkan pilihan : "; cin >> choose;
-        switch (choose){
-            case 1:
-                push();getch();
-                break;
-            case 2:
-                pop();getch();
-                break;
-            case 3:
-                clearr();
-                cout << "\nClear Berhasil" << endl;
-                getch();
-                break;
-            case 4:
-                cout << "Program Selesai :)" << endl;
-                getch();
-                break;
-                default:
-                cout << "Pilihan tidak ada\nMasukan pilihan yang benar" << endl;
-                getch();
-                break;
+    int pilih,maks;
+    int top=1;
+    cout<<"Masukkan jumlah maksimal barang yg ingin di kirim : ";
+    cin>>maks;
+    do
+    {
+        cout<<"======================="<<endl;
+        cout<<"||     MENU UTAMA    ||"<<endl;
+        cout<<"======================="<<endl;
+        cout<<"1. Tambah Data"<<endl;
+        cout<<"2. Hapus Data Terakhir"<<endl;
+        cout<<"3. Tampilkan Data"<<endl;
+        cout<<"4. Hapus Semua Data"<<endl;
+        cout<<"9. Keluar"<<endl;
+        cout<<"======================="<<endl;
+        cout<<"Pilihan Anda [1-9] : ";
+        cin>>pilih;
+        if(pilih==1)
+        {
+            if(top<=maks){
+                tambahdata(top);
+                top++;
+            }else{
+                cout<<"========================"<<endl;
+                cout<<"|| Barang Sudah Penuh ||"<<endl;
+                cout<<"========================"<<endl;
+                cout<<endl;
+            }
         }
-        system("cls");
-    }while(choose !=4);
+        if(pilih==2)
+        {
+            hapusdata(top-1);
+            top--;
+        }
+        if(pilih==3)
+        {
+            if(top>1){
+                tampildata(top);
+            }else{
+                cout<<"======================"<<endl;
+                cout<<"|| Barang kosong... ||"<<endl;
+                cout<<"======================"<<endl;
+                cout<<endl;
+            }
+        }
+        if(pilih==4)
+        {
+            top=1;
+            cout<<"============================================"<<endl;
+            cout<<"|| Semua data barang berhasil di hapus... ||"<<endl;
+            cout<<"============================================"<<endl;
+            cout<<endl;
+        }
+    }
+    while(pilih!=9);
+    return 0;
 }
